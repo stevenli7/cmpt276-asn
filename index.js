40612cc6e458583a80f8c48773a28381e3ea164b
Steven Li <stevenli6869@gmail.com>
2022-02-16 22:13:02 -0800

    rectangle database

--- index.js
+++ index.js
@@ -1,10 +1,89 @@
+const e = require('express')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg')
+var pool = new Pool({
+    connectionString: process.env.DATABASE_URL || "postgres://postgres:682269@localhost/cmpt276_a2"
+})
+
+var app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.get('/database', async (req, res) => {
+    try {
+        const result = await pool.query(`SELECT * FROM rectangles`)
+        const data = { results : result.rows }
+        res.render('pages/database', data)
+    } catch (error) {
+        res.end(error)
+    }
+})
+
+app.post('/rectangle/:id', async (req, res) => {
+    let id = req.params.id
+    try {
+        const result = await pool.query(`SELECT * FROM rectangles WHERE id = ${id}`)
+        let data = { results : result.rows }
+        res.render('pages/rectangle', data)
+    } catch (error) {
+        res.end(error)
+    }
+})
+
+app.post('/rectangle', async (req, res) => {
+    try {
+        let data = { results: [] }
+        res.render('pages/rectangle', data)
+    } catch (error) {
+        res.end(error)
+    }
+})
+
+app.post('/updateRectangle/:id', async (req, res) => {
+    let name = req.body.name
+    let width = req.body.width
+    let height = req.body.height
+    let color = req.body.color
+    let iq = req.body.iq
+    let id = req.params.id;
+    if (req.body.button == "delete") {
+        try {
+            await pool.query(`DELETE FROM rectangles WHERE id = ${id}`)
+            res.redirect('/database')
+        } catch (error) {
+            res.end(error)
+        }
+    } else if (req.body.button == "update") {
+        try {
+            await pool.query(`UPDATE rectangles SET name = '${name}', width = ${width}, height = ${height}, color = '${color}', iq = ${iq} WHERE id = ${id}`)
+            res.redirect('/database')
+        } catch (error) {
+            res.end(error)
+        }
+    }
+})
+
+app.post('/addRectangle', async (req, res) => {
+    let name = req.body.name
+    let width = req.body.width
+    let height = req.body.height
+    let color = req.body.color
+    let iq = req.body.iq
+    try {
+        await pool.query(`INSERT INTO rectangles (name, width, height, color, iq) VALUES ('${name}', ${width}, ${height}, '${color}', ${iq})`)
+        res.redirect('/database')
+    } catch (error) {
+        res.end(error)
+    }
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ public/scripts/rectangle.js
@@ -0,0 +1,52 @@
+var color = document.getElementById("color");
+var rectangleName = document.getElementById("rectangle-name");
+var rectangle = document.getElementById("image");
+var updateRectangles = document.getElementById("update");
+var inputs = document.querySelectorAll("input");
+
+window.onload = function display() {
+    if (rectangleName.value == "") {
+        document.title = "New Rectangle"
+        updateRectangles.innerHTML = "ADD"
+        updateRectangles.setAttribute("value", "add")
+        document.getElementById("information").action = "/addRectangle"
+        document.getElementById("delete").style.visibility = "hidden"
+    }
+    rectangle.style.setProperty("width", `${document.querySelector("input[name='width']").value}px`)
+    rectangle.style.setProperty("height", `${document.querySelector("input[name='height']").value}px`)
+    rectangle.style.setProperty("background-color", `${color.value}`)
+};
+
+inputs.forEach(element => element.addEventListener('input', updateValue))
+
+function updateValue(e) {
+    if (e.target.type == "number") {
+        if (isNaN(e.target.valueAsNumber)) {
+            alert("Please enter a number.")
+            e.target.value = e.target.defaultValue
+        } else if (e.target.value <= 0) {
+            alert("Value must be greater than 0");
+            e.target.value = e.target.defaultValue
+        } else if (e.target.name == "width") {
+            rectangle.style.setProperty("width", `${e.target.value}px`)
+        } else if (e.target.name == "height") {
+            rectangle.style.setProperty("height", `${e.target.value}px`)
+        }
+    } else if (e.target == color) {
+        rectangle.style.setProperty("background-color", `${e.target.value}`)
+    } else if (e.target == rectangleName) {
+        rectangle.innerHTML = e.target.value
+    }
+}
+
+updateRectangles.onclick = function checkValidForm() {
+    if (!CSS.supports("background-color", color.value)) {
+        alert("Invalid color");
+        return false;
+    }
+    if (rectangleName.value.trim() == "") {
+        alert("Enter a name");
+        return false;
+    }    
+    return true;
+}
